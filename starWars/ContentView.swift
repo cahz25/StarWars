895d9d6762ed291be4de361b2382be82285ae5f6
Cesar Hurtado <cesar.hurtado@globant.com>
2022-07-21 14:53:00 -0500

    Initial Commit

---
+++ starWars/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  starWars
+//
+//  Created by Cesar Augusto Hurtado Zapata on 21/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ starWars/starWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  starWarsApp.swift
+//  starWars
+//
+//  Created by Cesar Augusto Hurtado Zapata on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct starWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
